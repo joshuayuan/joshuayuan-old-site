cc5c6e7824df2e12930e32a9c385ccbef6afd19a
Joshua Yuan <joshua43210@gmail.com>
2017-07-23 15:23:42 -0700

    revamp the about page entirely

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleCoursework() {
+    var coursework = document.getElementById("coursework");
+    if (coursework.style.display === "none") {
+        coursework.style.display = "block";
+    } else {
+        coursework.style.display = "none";
+    }
+}
